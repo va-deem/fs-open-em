738e0048dcd812d3e7c310674edff380545e025f
Vadim S <vadim.stebakov@gmail.com>
2021-01-22 16:13:23 +0300

    Add frontend

---
+++  README.md
@@ -0,0 +1,2 @@
+[Link to deployed app](https://young-anchorage-82704.herokuapp.com)
+
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());
+app.use(express.static('build'));
 
 let persons = [
   {
